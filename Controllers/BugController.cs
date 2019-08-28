b51f507032cdeade52a76ece6529969553480a1b
Bart Calixto <bart@dot.uy>
2019-08-28 18:59:39 -0300

    initial commit

---
+++ Controllers/BugController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class BugController : Controller
+    {
+        public ActionResult Index([FromHeader]DateTimeOffset dt)
+        {
+            return Ok(dt);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ironhide.PromoNews.Api
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(x => x.MapRoute("a", "{controller=Home}/{action=Index}/{id?}"));
+        }
+    }
+}
